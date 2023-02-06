e605145ebf2605d80d21ccf3908b090ca8c9369b
Rabbi <Rabbyislam7777@gmail.com>
2023-02-06 23:56:34 +0600

    successfully added slider of image

--- app/src/main/java/com/example/collageappuser/MainActivity.java
+++ app/src/main/java/com/example/collageappuser/MainActivity.java
@@ -1,26 +1,86 @@
 package com.example.collageappuser;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private BottomNavigationView bottomNavigationView;
     private NavController navController;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_menu);
+
         bottomNavigationView = findViewById(R.id.buttomNavigationView);
         navController = Navigation.findNavController(this,R.id.frame_layout);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.start,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(this);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.navigation_video:
+                Toast.makeText(this, "video", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_developer:
+                Toast.makeText(this, "developer", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_rate:
+                Toast.makeText(this, "review", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_ebook:
+                Toast.makeText(this, "e-book", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_website:
+                Toast.makeText(this, "website", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_theme:
+                Toast.makeText(this, "theme", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_share:
+                Toast.makeText(this, "share", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/collageappuser/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/collageappuser/ui/home/HomeFragment.java
@@ -7,17 +7,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.collageappuser.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
+
+    ImageSlider imageSlider;
+    List<SlideModel> slideModels;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        imageSlider = view.findViewById(R.id.imageSlider);
+        slideModels = new ArrayList<SlideModel>();
+
+        slideModels.add(new SlideModel("https://picsum.photos/seed/picsum/200/300", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/id/237/200/300", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/200/300?grayscale", ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels);
+
+
+        return view;
     }
 }
