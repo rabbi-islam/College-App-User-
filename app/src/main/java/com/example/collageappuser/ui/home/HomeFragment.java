5127833bf223297e417bb0beded8de6eafc3e7ea
Rabbi <Rabbyislam7777@gmail.com>
2023-02-07 22:19:18 +0600

    map added

--- app/src/main/java/com/example/collageappuser/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/collageappuser/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.collageappuser.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -19,7 +21,8 @@ import java.util.List;
 
 
 public class HomeFragment extends Fragment {
-
+    
+    ImageView map;
     ImageSlider imageSlider;
     List<SlideModel> slideModels;
 
@@ -31,15 +34,31 @@ public class HomeFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
         imageSlider = view.findViewById(R.id.imageSlider);
+        map = view.findViewById(R.id.map);
         slideModels = new ArrayList<SlideModel>();
 
-        slideModels.add(new SlideModel("https://picsum.photos/seed/picsum/200/300", ScaleTypes.FIT));
-        slideModels.add(new SlideModel("https://picsum.photos/id/237/200/300", ScaleTypes.FIT));
-        slideModels.add(new SlideModel("https://picsum.photos/200/300?grayscale", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/seed/picsum/200/300","first picture represent the view of the world! ", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/id/237/200/300","second picture represent the view of the world! ", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/200/300?grayscale", "third picture represent the view of the world! ",ScaleTypes.FIT));
 
         imageSlider.setImageList(slideModels);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMap();
+            }
+        });
 
 
         return view;
+    }
+
+    private void openMap() {
+
+        Uri uri = Uri.parse("geo:0,0?q=daffodil international university,ashulia dhaka");
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
     }
 }
